0f7d84247646b30a4d75bed58bbf65b38ad8fb9f
Byrdann Fox <mobileupstate@gmail.com>
2014-12-16 15:33:41 -0500

    Updates...

---
+++ ByrdannFox, Inc/ByrdannFox, Inc/optionals.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+// var str = "Hello, playground"
+
+// has to be a String value...
+var nonOptionalMiddleName:String
+
+// can be either a String or nil value...
+var optionalMiddleName:String?
+

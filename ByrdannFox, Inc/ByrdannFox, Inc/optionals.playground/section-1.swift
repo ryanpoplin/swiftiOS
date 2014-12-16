bc364347427d51f9811f57e3d36c582e7accbc91
Byrdann Fox <mobileupstate@gmail.com>
2014-12-16 16:37:15 -0500

    Updates...

--- ByrdannFox, Inc/ByrdannFox, Inc/optionals.playground/section-1.swift
+++ ByrdannFox, Inc/ByrdannFox, Inc/optionals.playground/section-1.swift
@@ -1,6 +1,7 @@
 // Playground - noun: a place where people can play
 
 import UIKit
+import XCTest
 
 // var str = "Hello, playground"
 
@@ -9,4 +10,43 @@ var nonOptionalMiddleName:String
 
 // can be either a String or nil value...
 var optionalMiddleName:String?
+
+// forced unwrapping of an optional...
+var testOptional:Float? = 24.5
+println(testOptional)
+println(testOptional!)
+
+var theFirstName:String = "Byrdann"
+var theLastName:String? = nil
+var theFirstAndLastNames:String
+
+// always utilize optional binding to unwrap optionals...
+if let last = theLastName {
+    theFirstAndLastNames = theFirstName + " " + last
+    println("Last Name not nil...")
+} else {
+    theFirstAndLastNames = theFirstName
+    println("Last Name is nil...")
+}
+
+// Implicitly unwrapped optionals...
+var stupidName:String!
+
+//class MemberEntity {
+//    var location:LocationEntity? = nil
+//    var firstName:String = ""
+//    var lastName:String = ""
+//}
+//
+//class LocationEntity {
+//    var locationName = "Greenville"
+//    var locationDescription = "Byrdann Fox's home for now...."
+//    var locationId:Int32 = 5
+//}
+//
+//func testOptionalChaining() {
+//    var memberEntity = MemberEntity()
+//    var nameOfLocation:String? = memberEntity.location?.locationName
+//    XCTAssertNil(nameOfLocation)
+//}
 
